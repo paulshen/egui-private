9b0e9cd406d40667c2a1305fd4b779bc448cf586
Paul Shen <paulshen0@gmail.com>
2021-01-23 10:29:16 -0800

    add monaco typescript

--- my_app/src/app.rs
+++ my_app/src/app.rs
@@ -100,114 +100,114 @@ impl epi::App for MyApp {
   }
 }
 
-const SAMPLE_CODE: &str =
-  "export function createWorkInProgress(current: Fiber, pendingProps: any): Fiber {
-  let workInProgress = current.alternate;
-  if (workInProgress === null) {
-    // We use a double buffering pooling technique because we know that we'll
-    // only ever need at most two versions of a tree. We pool the other unused
-    // node that we're free to reuse. This is lazily created to avoid allocating
-    // extra objects for things that are never updated. It also allow us to
-    // reclaim the extra memory if needed.
-    workInProgress = createFiber(
-      current.tag,
-      pendingProps,
-      current.key,
-      current.mode,
-    );
-    workInProgress.elementType = current.elementType;
-    workInProgress.type = current.type;
-    workInProgress.stateNode = current.stateNode;
+const SAMPLE_CODE: &str = "// loading...";
+//   "export function createWorkInProgress(current: Fiber, pendingProps: any): Fiber {
+//   let workInProgress = current.alternate;
+//   if (workInProgress === null) {
+//     // We use a double buffering pooling technique because we know that we'll
+//     // only ever need at most two versions of a tree. We pool the other unused
+//     // node that we're free to reuse. This is lazily created to avoid allocating
+//     // extra objects for things that are never updated. It also allow us to
+//     // reclaim the extra memory if needed.
+//     workInProgress = createFiber(
+//       current.tag,
+//       pendingProps,
+//       current.key,
+//       current.mode,
+//     );
+//     workInProgress.elementType = current.elementType;
+//     workInProgress.type = current.type;
+//     workInProgress.stateNode = current.stateNode;
 
-    if (__DEV__) {
-      // DEV-only fields
-      workInProgress._debugID = current._debugID;
-      workInProgress._debugSource = current._debugSource;
-      workInProgress._debugOwner = current._debugOwner;
-      workInProgress._debugHookTypes = current._debugHookTypes;
-    }
+//     if (__DEV__) {
+//       // DEV-only fields
+//       workInProgress._debugID = current._debugID;
+//       workInProgress._debugSource = current._debugSource;
+//       workInProgress._debugOwner = current._debugOwner;
+//       workInProgress._debugHookTypes = current._debugHookTypes;
+//     }
 
-    workInProgress.alternate = current;
-    current.alternate = workInProgress;
-  } else {
-    workInProgress.pendingProps = pendingProps;
-    // Needed because Blocks store data on type.
-    workInProgress.type = current.type;
+//     workInProgress.alternate = current;
+//     current.alternate = workInProgress;
+//   } else {
+//     workInProgress.pendingProps = pendingProps;
+//     // Needed because Blocks store data on type.
+//     workInProgress.type = current.type;
 
-    // We already have an alternate.
-    // Reset the effect tag.
-    workInProgress.flags = NoFlags;
+//     // We already have an alternate.
+//     // Reset the effect tag.
+//     workInProgress.flags = NoFlags;
 
-    // The effect list is no longer valid.
-    workInProgress.nextEffect = null;
-    workInProgress.firstEffect = null;
-    workInProgress.lastEffect = null;
-    workInProgress.subtreeFlags = NoFlags;
-    workInProgress.deletions = null;
+//     // The effect list is no longer valid.
+//     workInProgress.nextEffect = null;
+//     workInProgress.firstEffect = null;
+//     workInProgress.lastEffect = null;
+//     workInProgress.subtreeFlags = NoFlags;
+//     workInProgress.deletions = null;
 
-    if (enableProfilerTimer) {
-      // We intentionally reset, rather than copy, actualDuration & actualStartTime.
-      // This prevents time from endlessly accumulating in new commits.
-      // This has the downside of resetting values for different priority renders,
-      // But works for yielding (the common case) and should support resuming.
-      workInProgress.actualDuration = 0;
-      workInProgress.actualStartTime = -1;
-    }
-  }
+//     if (enableProfilerTimer) {
+//       // We intentionally reset, rather than copy, actualDuration & actualStartTime.
+//       // This prevents time from endlessly accumulating in new commits.
+//       // This has the downside of resetting values for different priority renders,
+//       // But works for yielding (the common case) and should support resuming.
+//       workInProgress.actualDuration = 0;
+//       workInProgress.actualStartTime = -1;
+//     }
+//   }
 
-  // Reset all effects except static ones.
-  // Static effects are not specific to a render.
-  workInProgress.flags = current.flags & StaticMask;
-  workInProgress.childLanes = current.childLanes;
-  workInProgress.lanes = current.lanes;
+//   // Reset all effects except static ones.
+//   // Static effects are not specific to a render.
+//   workInProgress.flags = current.flags & StaticMask;
+//   workInProgress.childLanes = current.childLanes;
+//   workInProgress.lanes = current.lanes;
 
-  workInProgress.child = current.child;
-  workInProgress.memoizedProps = current.memoizedProps;
-  workInProgress.memoizedState = current.memoizedState;
-  workInProgress.updateQueue = current.updateQueue;
+//   workInProgress.child = current.child;
+//   workInProgress.memoizedProps = current.memoizedProps;
+//   workInProgress.memoizedState = current.memoizedState;
+//   workInProgress.updateQueue = current.updateQueue;
 
-  // Clone the dependencies object. This is mutated during the render phase, so
-  // it cannot be shared with the current fiber.
-  const currentDependencies = current.dependencies;
-  workInProgress.dependencies =
-    currentDependencies === null
-      ? null
-      : {
-          lanes: currentDependencies.lanes,
-          firstContext: currentDependencies.firstContext,
-        };
+//   // Clone the dependencies object. This is mutated during the render phase, so
+//   // it cannot be shared with the current fiber.
+//   const currentDependencies = current.dependencies;
+//   workInProgress.dependencies =
+//     currentDependencies === null
+//       ? null
+//       : {
+//           lanes: currentDependencies.lanes,
+//           firstContext: currentDependencies.firstContext,
+//         };
 
-  // These will be overridden during the parent's reconciliation
-  workInProgress.sibling = current.sibling;
-  workInProgress.index = current.index;
-  workInProgress.ref = current.ref;
+//   // These will be overridden during the parent's reconciliation
+//   workInProgress.sibling = current.sibling;
+//   workInProgress.index = current.index;
+//   workInProgress.ref = current.ref;
 
-  if (enableProfilerTimer) {
-    workInProgress.selfBaseDuration = current.selfBaseDuration;
-    workInProgress.treeBaseDuration = current.treeBaseDuration;
-  }
+//   if (enableProfilerTimer) {
+//     workInProgress.selfBaseDuration = current.selfBaseDuration;
+//     workInProgress.treeBaseDuration = current.treeBaseDuration;
+//   }
 
-  if (__DEV__) {
-    workInProgress._debugNeedsRemount = current._debugNeedsRemount;
-    switch (workInProgress.tag) {
-      case IndeterminateComponent:
-      case FunctionComponent:
-      case SimpleMemoComponent:
-        workInProgress.type = resolveFunctionForHotReloading(current.type);
-        break;
-      case ClassComponent:
-        workInProgress.type = resolveClassForHotReloading(current.type);
-        break;
-      case ForwardRef:
-        workInProgress.type = resolveForwardRefForHotReloading(current.type);
-        break;
-      default:
-        break;
-    }
-  }
+//   if (__DEV__) {
+//     workInProgress._debugNeedsRemount = current._debugNeedsRemount;
+//     switch (workInProgress.tag) {
+//       case IndeterminateComponent:
+//       case FunctionComponent:
+//       case SimpleMemoComponent:
+//         workInProgress.type = resolveFunctionForHotReloading(current.type);
+//         break;
+//       case ClassComponent:
+//         workInProgress.type = resolveClassForHotReloading(current.type);
+//         break;
+//       case ForwardRef:
+//         workInProgress.type = resolveForwardRefForHotReloading(current.type);
+//         break;
+//       default:
+//         break;
+//     }
+//   }
 
-  return workInProgress;
-}";
+//   return workInProgress;
+// }";
 
 fn syntax_highlighting(text: &str) -> ColoredText {
   ColoredText::text_with_extension(text, "js")
